45b4b7cec60f1a5948c3b74f020efb852e6fdcf2
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2021-06-05 14:15:49 +0200

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,23 @@
+
+
+const refs = {
+  sub: document.querySelector("[data-action='increment']"),
+  add: document.querySelector("[data-action='decrement']"),
+  span: document.querySelector('#value'),
+  wrapper: document.querySelector('#counter'),
+};
+
+let counterValue = 0;
+
+const increment = () => {
+  counterValue +=1;
+  document.getElementById("value").textContent = counterValue;
+};
+
+const decrement = () => {
+  counterValue -=1;
+  document.getElementById("value").textContent = counterValue;
+};
+
+refs.sub.addEventListener("click", increment);
+refs.add.addEventListener("click", decrement);
